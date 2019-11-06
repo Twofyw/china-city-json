f7117039a87a5afb247bd2858df9f22de610d891
Twofyw Yang <ywywywx@gmail.com>
2019-11-06 10:19:17 +0800

    done

--- format.py
+++ format.py
@@ -1,19 +1,33 @@
 import json
-import sys
+from pprint import pprint
+
+
+def get_cities_from_province_code(province_code, cities, districts):
+    province = cities[province_code]
+    # print(province)
+    province_name = province[0]['province']
+    children = []
+    for city in province:
+        name = city['name']
+        if name == '市辖区':  # use districts
+            children += [{'name': o['name']} for o in districts[city['id']]]
+        else:
+            children.append({'name': name})
+
+    return {'name': province_name, 'children': children}
 
 
 def main():
     with open('city.json') as f:
         cities = json.load(f)
 
-    result = []
-    for province_code in cities:
-        province = cities[province_code]
-        print(province)
-        province_name = province[0]['province']
-        cities_in_province = [o['name'] for o in province]
-        print(cities_in_province)
-        result.append({'name': province_name, 'children': ['' ]})
+    with open('county.json') as f:
+        districts = json.load(f)
+
+    result = [get_cities_from_province_code(province_code, cities, districts) for province_code in cities]
+    with open('cities_ywx_format.json', 'w') as f:
+        json.dump(result, f, ensure_ascii=False)
+    pprint(result)
 
 
 if __name__ == '__main__':
